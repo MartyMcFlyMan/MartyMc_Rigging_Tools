3ec3a0f5771471a6626f0ab402a5dee6b0e959ee
Jérôme Collin <j.collin2315@gmail.com>
2021-03-30 11:07:07 -0400

    Create fastOrthoCam.py
    Simple script to quickly snap persp camera to closest orthographic view

---
+++ other_tools/fastOrthoCam.py
@@ -0,0 +1,18 @@
+import maya.cmds as cmds
+
+# Simple script for fast orthographic view, put this in a shortcut or something
+
+isOrtho = cmds.getAttr('persp' + '.orthographic')
+
+if isOrtho:
+    cmds.setAttr('persp' + '.orthographic', 0)
+
+else:
+    camRX, camRY, camRZ = cmds.xform('persp', q=True, ro=True)
+
+    newRX = 90 * round((camRX % 360)/90)
+    newRY = 90 * round((camRY % 360)/90)
+
+    cmds.xform('persp', ro=(newRX, newRY, camRZ))
+
+    cmds.setAttr('persp' + '.orthographic', 1)
