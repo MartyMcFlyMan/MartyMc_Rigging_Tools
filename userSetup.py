845605e63efea5d63b497469b45db518214192e1
Jerome Collin <j.collin2315@gmail.com>
2021-11-16 16:21:20 -0500

    added small scripts path for maya startup and added helper comment

--- userSetup.py
+++ userSetup.py
@@ -1,6 +1,9 @@
+#add this to your userSetup.py in Drive:/Users/User/Documents/maya/version/prefs/scripts/
+
 import maya.cmds as cmds
 import sys
 
 scripts_path = cmds.internalVar(usd=True)
 sys.path.append(scripts_path.rpartition('scripts')[0] + 'prefs/scripts/MM_tools/')
+sys.path.append(scripts_path.rpartition('scripts')[0] + 'prefs/scripts/MM_tools/small_scripts_and_wips')
 sys.path.append(scripts_path.rpartition('scripts')[0] + 'prefs/scripts/other_tools/')
