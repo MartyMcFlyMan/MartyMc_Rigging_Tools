74dc9ba7c1ff5127586ae1ba2256f983e3850d78
Jerome Collin <j.collin2315@gmail.com>
2021-11-18 10:48:24 -0500

    cleanup

--- MM_tools/sandbox/maya_api_tutorial.py
+++
@@ -1,6 +0,0 @@
-import maya.OpenMaya  # classes for defining nodes and commands
-import maya.OpenMayaUI  # classes for creating uis
-import maya.OpenMayaAnim  # classes for animation, including deformers and IK
-import maya.OpenMayaFX  # classes for Autodesk Dynamics
-import maya.OpenMayaRender  # classes for render functions
-
